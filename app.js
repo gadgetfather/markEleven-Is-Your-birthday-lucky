15f7c0f4574b73068135fbd5d194f1dd784c8aae
gadgetfather <kamblea635@gmail.com>
2021-09-14 10:16:12 +0530

    update app.js
    added queryselectors

---
+++ app.js
@@ -0,0 +1,3 @@
+const dateOfBirth = document.querySelector("#date-of-birth")
+const luckyNumber = document.querySelector("#lucky-number")
+const checkNumber = document.querySelector("#check-number")
