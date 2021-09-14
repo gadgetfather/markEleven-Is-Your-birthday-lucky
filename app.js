c5e2d253c8e60227d5d26382fff6cdc9e04fafc2
gadgetfather <kamblea635@gmail.com>
2021-09-14 10:46:23 +0530

    Update app.js
    created function to sum the DOB

--- app.js
+++ app.js
@@ -3,7 +3,25 @@ const luckyNumber = document.querySelector("#lucky-number")
 const checkNumber = document.querySelector("#check-number")
 
 
-checkNumber.addEventListener('click',function getValue(){
-    console.log(dateOfBirth.value)
-    console.log(luckyNumber.value)
-})
+function checkBirthDateIsLucky(){
+ const dob = dateOfBirth.value
+ const sum = calculateSum(dob)
+ console.log(sum)
+}
+
+
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","")
+    let sum = 0;
+    for(let i = 0; i<dob.length; i++){
+        sum = sum + Number(dob.charAt(i))
+
+    }
+    return sum;
+
+}
+
+
+
+checkNumber.addEventListener('click',checkBirthDateIsLucky)
