d084370bdc4482e1f0743e928638322f1c937fa9
gadgetfather <kamblea635@gmail.com>
2021-09-14 11:54:19 +0530

    Update app.js
    added proccesing part

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const dateOfBirth = document.querySelector("#date-of-birth")
 const luckyNumber = document.querySelector("#lucky-number")
 const checkNumber = document.querySelector("#check-number")
+const outputBox = document.querySelector("#output-box")
 
 
 function checkBirthDateIsLucky(){
  const dob = dateOfBirth.value
  const sum = calculateSum(dob)
- console.log(sum)
+ if(sum&&dob){
+ checkLuck(sum,luckyNumber.value)}
+ else{
+     outputBox.textContent = "Enter both values !!!"
+
+ }
 }
 
+
+function checkLuck(sum,luckyNumber){
+    if(sum%luckyNumber===0){
+        outputBox.textContent="YOUR BIRTHDAY IS LUCKY!"
+
+    }
+    else{
+        outputBox.textContent="YOUR BIRTHDAY IS NOT LUCKY"
+
+    }
+    
+}
 
 
 function calculateSum(dob){
