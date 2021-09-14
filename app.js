dd069a0d42666ece8044e71939b9925fe3ef1821
gadgetfather <kamblea635@gmail.com>
2021-09-14 10:20:20 +0530

    Update app.js
    consoled the values

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const dateOfBirth = document.querySelector("#date-of-birth")
 const luckyNumber = document.querySelector("#lucky-number")
 const checkNumber = document.querySelector("#check-number")
+
+
+checkNumber.addEventListener('click',function getValue(){
+    console.log(dateOfBirth.value)
+    console.log(luckyNumber.value)
+})
